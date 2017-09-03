71269244c27025d3c0ae03af0350a83970e26d30
Li Teng <930372551@qq.com>
2017-09-03 13:07:12 +0800

    first commit

---
+++ GeoAPI/GeoAPI/Consts.cs
@@ -0,0 +1,14 @@
+internal static class Consts
+{
+    public const string Title = "GeoAPI";
+    public const string Description = "GeoAPI";
+    public const string Company = "NetTopologySuite - Team";
+    public const string Copyright = "Copyright © NetTopologySuite - Team 2007-2017";
+    public const bool ComVisible = false;
+    public const bool CLSCompliant = true;
+
+    private const string FullFrameworkGuid = "b6726fc4-0319-4a6d-84f5-aafc6ba530e3";
+    public const string Configuration = "Stable";
+    public const string Product = "GeoAPI.Net40";
+    public const string Guid = FullFrameworkGuid;
+}
